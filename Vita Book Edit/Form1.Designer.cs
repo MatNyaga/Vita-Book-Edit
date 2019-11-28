de706ee589f85bfcd43f3934478b44717fad64f0
Martin Nyaga <MatNyaga@gmail.com>
2019-11-29 00:24:23 +0300

    Added a progress bar while copying the EPUB file.

---
+++ Vita Book Edit/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Vita_Book_Edit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.library = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.AddBook = new System.Windows.Forms.Button();
+            this.librarypathlabel = new System.Windows.Forms.Label();
+            this.LibraryBookNumber = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // library
+            // 
+            this.library.Location = new System.Drawing.Point(39, 23);
+            this.library.Name = "library";
+            this.library.Size = new System.Drawing.Size(137, 23);
+            this.library.TabIndex = 0;
+            this.library.Text = "Library Location";
+            this.library.UseVisualStyleBackColor = true;
+            this.library.Click += new System.EventHandler(this.library_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "My Vita Book Library";
+            // 
+            // AddBook
+            // 
+            this.AddBook.Location = new System.Drawing.Point(39, 104);
+            this.AddBook.Name = "AddBook";
+            this.AddBook.Size = new System.Drawing.Size(90, 105);
+            this.AddBook.TabIndex = 2;
+            this.AddBook.Text = "+";
+            this.AddBook.UseVisualStyleBackColor = true;
+            this.AddBook.Click += new System.EventHandler(this.AddBook_Click);
+            // 
+            // librarypathlabel
+            // 
+            this.librarypathlabel.AutoSize = true;
+            this.librarypathlabel.Location = new System.Drawing.Point(244, 28);
+            this.librarypathlabel.Name = "librarypathlabel";
+            this.librarypathlabel.Size = new System.Drawing.Size(16, 13);
+            this.librarypathlabel.TabIndex = 3;
+            this.librarypathlabel.Text = "...";
+            // 
+            // LibraryBookNumber
+            // 
+            this.LibraryBookNumber.AutoSize = true;
+            this.LibraryBookNumber.Location = new System.Drawing.Point(513, 58);
+            this.LibraryBookNumber.Name = "LibraryBookNumber";
+            this.LibraryBookNumber.Size = new System.Drawing.Size(58, 13);
+            this.LibraryBookNumber.TabIndex = 4;
+            this.LibraryBookNumber.Text = "( 0 Books )";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LibraryBookNumber);
+            this.Controls.Add(this.librarypathlabel);
+            this.Controls.Add(this.AddBook);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.library);
+            this.Name = "Form1";
+            this.Text = "Vita Book Library";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button library;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button AddBook;
+        private System.Windows.Forms.Label librarypathlabel;
+        private System.Windows.Forms.Label LibraryBookNumber;
+    }
+}
+
---
+++ Vita Book Edit/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vita_Book_Edit
+{
+    public partial class Form1 : Form
+    {
+        String LibraryLocation = null;
+        String ReaderTitle = "PCSC80012";
+        String DLCBookTitle = "00000000BLKSTOK1";
+        int BookNumber = 2;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void library_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                LibraryLocation = folderBrowserDialog1.SelectedPath;
+                System.IO.Directory.CreateDirectory(LibraryLocation+"\\"+ReaderTitle+"\\"+DLCBookTitle);
+                librarypathlabel.Text = LibraryLocation + "\\";
+
+                //Find the Number of Books (Count folders with book in them)
+                const string searchQuery = "*" + "book" + "*";
+                var directory = new DirectoryInfo(LibraryLocation + "\\" + ReaderTitle + "\\" + DLCBookTitle);
+                var directories = directory.GetDirectories(searchQuery, SearchOption.AllDirectories);
+                foreach (var d in directories)
+                {
+                    BookNumber+=1;
+                }
+                
+                LibraryBookNumber.Text = "( "+(BookNumber-1)+" Books )";
+            }
+
+        }
+
+        private void AddBook_Click(object sender, EventArgs e)
+        {
+            if (BookNumber < 10)
+            {
+                System.IO.Directory.CreateDirectory(LibraryLocation + "\\" + ReaderTitle + "\\" + DLCBookTitle + "\\" + ("book0" + BookNumber));
+                BookNumber += 1;
+                LibraryBookNumber.Text = "( " + (BookNumber) + " Books )";
+                BookNumber -= 1;
+                AddBook mybook = new AddBook(LibraryLocation + "\\" + ReaderTitle + "\\" + DLCBookTitle + "\\" + ("book0" + BookNumber));
+                mybook.ShowDialog();
+            }
+            else {
+                System.IO.Directory.CreateDirectory(LibraryLocation + "\\" + ReaderTitle + "\\" + DLCBookTitle + "\\" + ("book" + BookNumber));
+                BookNumber += 1;
+                LibraryBookNumber.Text = "( " + (BookNumber) + " Books )";
+                BookNumber -= 1;
+                AddBook mybook = new AddBook(LibraryLocation + "\\" + ReaderTitle + "\\" + DLCBookTitle + "\\" + ("book" + BookNumber));
+                mybook.ShowDialog();
+            }
+            
+        }
+    }
+}
